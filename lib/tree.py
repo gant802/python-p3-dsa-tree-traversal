7fcb0d9a3d93ae2a4f77e871e2befdc680da8a75
Grant Cummings <grantcummings4429@gmail.com>
2024-05-23 17:45:02 -0400

    finished exercise

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,25 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    result = []
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop(0)
+      if node['id'] == id:
+        return node
+      result.append(node)
+      nodes_to_visit = node['children'] + nodes_to_visit #! Depth first
+      #nodes_to_visit = nodes_to_visit + node['children']  #! Breadth first
+
+    return None
+
+
+
+
+
+
+
+
+
+
